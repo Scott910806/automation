dee4954414702fa0d61037aad573d3aad88fe2ea
yxma2010 <yxma2010@shuhe.com>
2023-04-25 17:28:03 +0800

    clean workspace

--- src/test/java/cases/TranslateTest.java
+++ src/test/java/cases/TranslateTest.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import mytools.HEstrategy;
 import mytools.MyUtils;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -23,13 +22,14 @@ public class TranslateTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"src/test/java/data/save/simpleHeel.json",
+    @ValueSource(strings = {
             "src/test/java/data/save/built_ins_for_string.json",
             "src/test/java/data/save/built_ins_for_number.json",
             "src/test/java/data/save/built_ins_for_collections.json",
             "src/test/java/data/save/built_ins_for_type_check_and_conversion.json",
             "src/test/java/data/save/heel_for_LINQ.json",
-            "src/test/java/data/save/heel_samples.json"})
+            "src/test/java/data/save/heel_samples.json"
+    })
     public void translate(String path) throws Exception{
         String payload = MyUtils.readJsonFile(path);
         Response response = hEstrategy.save(payload);
