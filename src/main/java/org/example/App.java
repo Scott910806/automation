37198c389b602468953fc10a5e8a1d6ab15d4187
yxma2010 <yxma2010@shuhe.com>
2023-04-25 17:42:38 +0800

    clean

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.List;
-
 /**
  * Hello world!
  *
@@ -10,17 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        List<String> list = List.of("we,  are;;  second ;  to     none".split("[\\,\\;\\s]+"));
-        for (String s : list) {
-            System.out.println(s);
-        }
-        boolean bool = "L  A".matches("L\\s{2}A");
-        System.out.println(bool);
-
-         String p_n = "黄宏贵";
-         String p_u = "fa3d363c-426f-4575-ab34-d3bf45755df5";
-         String baseUrl = "http://hengine.apps01.ali-bj-sit03.shuheo.net/hengine";
-        System.out.println(baseUrl + "?p_u=" + p_u + "&p_n=" + p_n);
+        System.out.println("hello world");
     }
 
 }
